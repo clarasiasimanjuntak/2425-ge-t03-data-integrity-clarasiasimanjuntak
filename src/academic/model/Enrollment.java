030d6359f21ffa71e3e32fc33ea9fd7e656c3ef6
Clarasia simanjuntak <143679140+clarasiasimanjuntak@users.noreply.github.com>
2025-02-18 10:04:08 +0700

    pair 16

--- src/academic/model/Enrollment.java
+++ src/academic/model/Enrollment.java
@@ -1,11 +1,29 @@
 package academic.model;
 
-/**
- * @author NIM Nama
- * @author NIM Nama
- */
 public class Enrollment {
+    private final String courseCode;
+    private final String studentId;
+    private final String academicYear;
+    private final String semester;
+    private final String grade;
 
-    // class definition
+    public Enrollment(String courseCode, String studentId, String academicYear, String semester, String grade) {
+        this.courseCode = courseCode;
+        this.studentId = studentId;
+        this.academicYear = academicYear;
+        this.semester = semester;
+        this.grade = grade;
+    }
 
+    public String getCourseCode() {
+        return courseCode;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public String toString() {
+        return courseCode + "|" + studentId + "|" + academicYear + "|" + semester + "|" + grade;
+    }
 }
